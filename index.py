fdd26c1d87b262e831845f09852fbac4f104f42a
junghwan.shin <shjohw11@naver.com>
2025-02-20 22:49:47 +0900

    API 작성

--- index.py
+++ index.py
@@ -1,21 +1,19 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 import json
 
+class_1 = {"category_id": 1, "class_id": 1, "due_date": '2025-07-12', "loc": "제주시 이도2동", "now_participants": 2, "max_participants": 10, "how_much": 15000, "teacher_name": "해녀 김바당", "teacher_article": "20년 경력 바다의 이야기꾼", "class_name": "해녀의 바당요리", "class_article": "해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요", "class_detail": "제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.", "is_closed": False}
+class_2 = {"category_id": 1, "class_id": 2, "due_date": '2025-07-12', "loc": "제주시 이도2동", "now_participants": 2, "max_participants": 10, "how_much": 15000, "teacher_name": "해녀 김바당", "teacher_article": "20년 경력 바다의 이야기꾼", "class_name": "해녀의 바당요리", "class_article": "해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요", "class_detail": "제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.", "is_closed": False}
+class_3 = {"category_id": 1, "class_id": 3, "due_date": '2025-07-12', "loc": "제주시 이도2동", "now_participants": 2, "max_participants": 10, "how_much": 15000, "teacher_name": "해녀 김바당", "teacher_article": "20년 경력 바다의 이야기꾼", "class_name": "해녀의 바당요리", "class_article": "해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요", "class_detail": "제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.", "is_closed": False}
+class_4 = {"category_id": 1, "class_id": 4, "due_date": '2025-07-12', "loc": "제주시 이도2동", "now_participants": 2, "max_participants": 10, "how_much": 15000, "teacher_name": "해녀 김바당", "teacher_article": "20년 경력 바다의 이야기꾼", "class_name": "해녀의 바당요리", "class_article": "해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요", "class_detail": "제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.", "is_closed": False}
+class_5 = {"category_id": 1, "class_id": 5, "due_date": '2025-07-12', "loc": "제주시 이도2동", "now_participants": 2, "max_participants": 10, "how_much": 15000, "teacher_name": "해녀 김바당", "teacher_article": "20년 경력 바다의 이야기꾼", "class_name": "해녀의 바당요리", "class_article": "해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요", "class_detail": "제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.", "is_closed": False}
+class_6 = {"category_id": 1, "class_id": 6, "due_date": '2025-07-12', "loc": "제주시 이도2동", "now_participants": 2, "max_participants": 10, "how_much": 15000, "teacher_name": "해녀 김바당", "teacher_article": "20년 경력 바다의 이야기꾼", "class_name": "해녀의 바당요리", "class_article": "해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요", "class_detail": "제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.", "is_closed": False}
+    
+class_list = [class_1, class_2, class_3, class_4, class_5, class_6]
+    
+category_dic = {
+    1: class_list
+}
 
-def make_class_list():
-    class_1 = {"category_id": 1, "class_id": 1, "due_date": '2025-07-12', "loc": "제주시 이도2동", "now_participants": 2, "max_participants": 10, "how_much": 15000, "teacher_name": "해녀 김바당", "teacher_article": "20년 경력 바다의 이야기꾼", "class_name": "해녀의 바당요리", "class_article": "해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요", "class_detail": "제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.", "is_closed": False}             #" "
-    class_2 = {"category_id": 1, "class_id": 2, "due_date": '2025-07-12', "loc": "제주시 이도2동", "now_participants": 2, "max_participants": 10, "how_much": 15000, "teacher_name": "해녀 김바당", "teacher_article": "20년 경력 바다의 이야기꾼", "class_name": "해녀의 바당요리", "class_article": "해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요", "class_detail": "제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.", "is_closed": False}
-    class_3 = {"category_id": 1, "class_id": 3, "due_date": '2025-07-12', "loc": "제주시 이도2동", "now_participants": 2, "max_participants": 10, "how_much": 15000, "teacher_name": "해녀 김바당", "teacher_article": "20년 경력 바다의 이야기꾼", "class_name": "해녀의 바당요리", "class_article": "해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요", "class_detail": "제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.", "is_closed": False}
-    class_4 = {"category_id": 1, "class_id": 4, "due_date": '2025-07-12', "loc": "제주시 이도2동", "now_participants": 2, "max_participants": 10, "how_much": 15000, "teacher_name": "해녀 김바당", "teacher_article": "20년 경력 바다의 이야기꾼", "class_name": "해녀의 바당요리", "class_article": "해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요", "class_detail": "제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.", "is_closed": False}
-    class_5 = {"category_id": 1, "class_id": 5, "due_date": '2025-07-12', "loc": "제주시 이도2동", "now_participants": 2, "max_participants": 10, "how_much": 15000, "teacher_name": "해녀 김바당", "teacher_article": "20년 경력 바다의 이야기꾼", "class_name": "해녀의 바당요리", "class_article": "해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요", "class_detail": "제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.", "is_closed": False}
-    class_6 = {"category_id": 1, "class_id": 6, "due_date": '2025-07-12', "loc": "제주시 이도2동", "now_participants": 2, "max_participants": 10, "how_much": 15000, "teacher_name": "해녀 김바당", "teacher_article": "20년 경력 바다의 이야기꾼", "class_name": "해녀의 바당요리", "class_article": "해녀의 불턱에서 성게국과 옥돔구이를 만드는 소중한 시간을 함께 해요", "class_detail": "제주의 인심이 담겨 있다는 별미 성게국과 임금님께 진상되던 옥돔을 직접 구워 맛보면 제주의 바다가 펼쳐질거에요.", "is_closed": False}
-    
-    class_list = [class_1, class_2, class_3, class_4, class_5, class_6]
-    
-    return class_list
-
-    
-    
 '''
 class CustomHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -31,11 +29,19 @@ class CustomHandler(SimpleHTTPRequestHandler):
 
 class CustomHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain; charset=utf-8')
-        self.end_headers()
-        self.wfile.write("Hello krampoline!".encode('utf-8'))
-
+        if self.path == '/hello':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            response = "Hello, world!"
+            self.wfile.write(json.dumps(response).encode('utf-8'))
+        elif re.match(r'^/api/category/\d+$', self.path):
+            category_id = self.path.split('/')[-1]
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            response = category_dic[category_id]
+            self.wfile.write(json.dumps(response).encode('utf-8'))
 
 PORT = 8000
 
