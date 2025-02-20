b5e5d695f7cad170ee3c3064e0b699adde982f4e
jeiden05 <jeiden05@naver.com>
2025-02-20 22:08:04 +0900

    .

---
+++ myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+'''
+path('categories/<id>/', views.category_classlist),
+path('classinfo/<id>/', views.class_info),
+path('appliedclass/', views.applied_class)
+'''
